bbfb5410627634c4356dc6066f78908a728ea0ac
Oleg <pakolegdev@gmail.com>
2022-11-06 23:19:13 +0300

    add @EnableJpaAuditing support

--- app/src/main/java/com/epam/esm/Application.java
+++ app/src/main/java/com/epam/esm/Application.java
@@ -2,8 +2,10 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
